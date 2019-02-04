42df7bf15bf536077eaa3c32e27ec206d23d640d
Lachin99 <47322589+Lachin99@users.noreply.github.com>
2019-02-04 12:33:40 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,14 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function Mostrar()
 {	
-	
+var nombre;	
+var edad;
+nombre= document.getElementById("elNombre").value;
+edad= document.getElementById("laEdad").valeu;
+alert("su nombre es " + nombre + " y tiene " + edad + " años");
+
+
+
+
 }
 
